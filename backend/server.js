a227fac3574c2eea83559744352b4aa0fbfe5e0a
johnnikkodc2 <johnnikko.delacruz.cics@ust.edu.ph>
2023-12-06 07:37:58 +0800

    push

--- backend/server.js
+++ backend/server.js
@@ -20,11 +20,15 @@ const { Tune } = require("@mui/icons-material");
 
 const app = express();
 const port = process.env.PORT || 3001;
+const corsOptions = {
+  origin: "https://jmig-gravasend-frontend.onrender.com",
+  optionsSuccessStatus: 200,
+};
 
 app.use(cors());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
+app.use(cors(corsOptions));
 app.use(itemsRouter);
 app.use(adminRouter);
 app.use(mobileRouter);
